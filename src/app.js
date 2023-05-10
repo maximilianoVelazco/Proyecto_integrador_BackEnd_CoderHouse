e38851a15818616342609b09b0293dd99450210b
Maxi Velazco <maximilianovelazco.mv@gmail.com>
2023-05-09 21:13:13 -0300

    entrega servidor con express

--- src/app.js
+++ src/app.js
@@ -11,7 +11,6 @@ app.get('/products', async (req, res) =>{
     res.send(productos);
 })
 
-
 app.get('/products', async (req, res) =>{
     let productos = await productManager.getProducts();
     let { limit } = req.query;
@@ -28,13 +27,3 @@ app.get('/products/:pid', async (req, res) =>{
 
 const server = app.listen(8080, () => console.log('Server running on port 8080'));
 
-/*El servidor debe contar con los siguientes endpoints:
-ruta ‘/products’, la cual debe leer el archivo de productos y devolverlos dentro de un objeto. 
-
-Agregar el soporte para recibir por query param el valor ?limit= el cual recibirá un límite de resultados.
-Si no se recibe query de límite, se devolverán todos los productos
-Si se recibe un límite, sólo devolver el número de productos solicitados
-
-
-
- */
